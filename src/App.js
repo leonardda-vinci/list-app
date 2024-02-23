a1bcf39258490ec5e036e797498b666d0f96e2b5
Kathrina Valdezco <kathvaldezcoronquillo@gmail.com>
2024-02-24 03:53:01 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef,CSSProperties  } from 'react';
+import React, { useState, useEffect, useRef,Suspense, lazy   } from 'react';
 import axios from 'axios';
-import LazyLoad from 'react-lazyload';
+
 import { Row, Col } from 'react-bootstrap';
 
 import ClipLoader from "react-spinners/ClipLoader";
@@ -10,7 +10,10 @@ const App = () => {
   const [tableData, setTableData] = useState([]);
   const [hasMore, setHasMore] = useState(true);
   const [page, setPage] = useState(1);
-
+const Loading = () => {
+  <ClipLoader/>
+}
+  
   const loaderRef = useRef(null);
 
   const fetchData = async () => {
@@ -49,7 +52,7 @@ const App = () => {
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 1,
+      threshold: 0.5,
     };
   
     const observer = new IntersectionObserver(handleIntersection, options);
@@ -65,7 +68,7 @@ const App = () => {
       }
     };
   }, [loaderRef, hasMore ]);
-console.log("hasMore"+ hasMore)
+
   return (
       <div className="app-container">
       <div className="search-bar-container">
@@ -76,29 +79,29 @@ console.log("hasMore"+ hasMore)
           {tableData.map((launch, index) => (
           <Row className="tableData-descriptions" key={launch.id} style={{ flexWrap: 'nowrap' }}>
           <Col id="image-content" lg={6}>
-           <LazyLoad height={200} offset={100} once>
+          <Suspense fallback={Loading}>
            <img
               src={launch.links.patch.small}
               alt="Large Patch"
               className="img-fluid"
               loading="lazy"
             />
-            </LazyLoad>
+            </Suspense>
             </Col>
             
             <Col lg={6}>
-            <LazyLoad height={200} offset={100} once>
-            <p id="tableData-Header">{launch.flight_number}: {launch.name} ({launch.date_local.slice(0, 4)})</p>
-            <p id="tableData-details">Details: {launch.details}</p>
-            </LazyLoad>
+            <Suspense fallback={Loading}>
+                <p id="tableData-Header">{launch.flight_number}: {launch.name} ({launch.date_local.slice(0, 4)})</p>
+                <p id="tableData-details">Details: {launch.details}</p>
+              </Suspense>
            </Col>
            </Row>
            ))}
        
         {hasMore ? (
          <div style={{ textAlign: 'center', padding: '20px' }} ref={loaderRef}>
-        {/* Display your loader here */}
-        <ClipLoader className="loader" />
+      
+        <ClipLoader/>
       </div>
       ) : (
       <p style={{ textAlign: 'center', padding: '20px' }}>
